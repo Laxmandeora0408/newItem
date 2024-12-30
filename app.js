d22b74e7c4403456f323e9d4603fc89b5c409ea3
Laxman Deora <laxmandeora0408>
2024-12-30 15:43:10 +0530

    new

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-if(process.env.NODE_ENV!="production"){
-  require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
-console.log(process.env.SECERET)
+console.log(process.env.SECERET);
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const methodOverride = require('method-override')
-const MongoStore = require('connect-mongo');
+const methodOverride = require("method-override");
+const MongoStore = require("connect-mongo");
 const path = require("path");
-// const MONGO_URL = "mongodb://127.0.0.1:27017/bulk"; 
+// const MONGO_URL = "mongodb://127.0.0.1:27017/bulk";
 const dbUrl = process.env.ATLASDB_URL;
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
@@ -27,24 +27,24 @@ const Listing = require("./models/listing.js");
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: process.env.SECERET
+    secret: process.env.SECERET,
   },
-  touchAfter: 24*3600
-})
+  touchAfter: 24 * 3600,
+});
 
-store.on("error",()=>console.log("error in mongo session store"));
+store.on("error", () => console.log("error in mongo session store"));
 
 const sessionOption = {
-  store:store,
+  store: store,
   secret: process.env.SECERET,
   resave: false,
   saveUninitialized: true,
-  cookie:{
-    expires: Date.now() + 7*24*60*60*1000,
-    maxAge: 7*24*60*60*1000,
-    httpOnly:true
-  }
-}
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 main()
   .then(() => {
@@ -62,11 +62,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-
-
-
 
 app.use(session(sessionOption));
 app.use(flash());
@@ -77,31 +74,27 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   next();
-})
-
-app.use("/listing",listingRouter);
-app.use("/listing/:id/reviews",reviewRouter);
-app.use("/",userRouter);
-app.use("/",filterRouter)
-
-
-
-app.all("*",wrapAsync, async(req,res,next)=>{
-  next(new ExpressError(404,"page not found!"));
 });
 
-app.use((err,req,res,next) =>{
-  let {statusCode= 500,message = "something went wrong!"}= err;
-  res.status(statusCode).render("error.ejs",{message});
+app.use("/listing", listingRouter);
+app.use("/listing/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+app.use("/", filterRouter);
+
+app.all("*", wrapAsync, async (req, res, next) => {
+  next(new ExpressError(404, "page not found!"));
 });
 
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+});
 
 app.listen(8080, () => {
-  console.log("server is listening to port 8080"); 
+  console.log("server is listening to port 8080");
 });
